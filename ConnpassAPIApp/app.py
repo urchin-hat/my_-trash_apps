bc438e8e7955d1d49cec97fe08305a83a05e7229
urchin-hat <s.fukushima1984x.xunil@gmail.com>
2018-07-30 22:30:51 +0900

    add setthing uwsgi

--- ConnpassAPIApp/app.py
+++ ConnpassAPIApp/app.py
@@ -48,4 +48,4 @@ def post_index():
     return render_template('top.html',title=title, result=result_dict)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
